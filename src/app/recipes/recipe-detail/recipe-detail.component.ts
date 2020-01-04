4a6801226e4631adc6b3e2704852505e247e29b1
coderkarma <karmadrukpa11@gmail.com>
2020-01-03 16:06:10 -0800

    added shopping-list service functionality to add the recipe item when click on the add add ingredient button

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,16 +1,20 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { Recipe } from '../recipe.model';
+import { Component, OnInit, Input } from "@angular/core";
+import { Recipe } from "../recipe.model";
+import { RecipeService } from "../recipe.service";
 
 @Component({
-  selector: 'app-recipe-detail',
-  templateUrl: './recipe-detail.component.html',
-  styleUrls: ['./recipe-detail.component.css']
+  selector: "app-recipe-detail",
+  templateUrl: "./recipe-detail.component.html",
+  styleUrls: ["./recipe-detail.component.css"]
 })
 export class RecipeDetailComponent implements OnInit {
-@Input() recipe: Recipe;
-  constructor() { }
+  @Input() recipe: Recipe;
 
-  ngOnInit() {
+  constructor(private recipeServie: RecipeService) {}
+
+  ngOnInit() {}
+
+  onAddToShoppingList() {
+    this.recipeServie.addIngredientsToShoppingList(this.recipe.ingredients);
   }
-
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,23 +1,35 @@
-import { EventEmitter } from "@angular/core";
+import { EventEmitter, Injectable } from "@angular/core";
 import { Recipe } from "./recipe.model";
+import { Ingredient } from "../shared/ingredient.model";
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
+@Injectable()
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
   private recipes: Recipe[] = [
     new Recipe(
-      "A test Recipe",
-      "This is just a test recipe",
-      "https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg"
+      "A Burger",
+      "A Tasty Burger",
+      "https://i.pinimg.com/474x/b0/62/53/b06253c4792ddefd0b5b06ace973de31--popular-photography-burgers.jpg",
+      [new Ingredient("Meat", 1), new Ingredient("French Fires", 20)]
     ),
     new Recipe(
-      "Another test Recipe",
-      "This is just a test recipe",
-      "https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg"
+      "Giant Burger",
+      "Delicious",
+      "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/beyond-burger-nutrition-and-ingredients-1569351590.jpg?crop=0.752xw:1.00xh;0.107xw,0&resize=480:*",
+      [new Ingredient("Buns", 2), new Ingredient("Meat", 1)]
     )
   ];
 
+  constructor( private slService: ShoppingListService){
+
+  }
   getRecipes() {
     // use slice to get newArray of recipe, since array is referrenced typed
     return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList (ingredients: Ingredient[]){
+   this.slService.addIngredients(ingredients);
   }
 }
